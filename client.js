78e9d146ae3e2c454252e52edc24f739dc0266ed
SeYmUr867543210 <64531437+SeYmUr867543210@users.noreply.github.com>
2021-07-03 21:12:25 +0500

    .

--- client.js
+++ client.js
@@ -1,11 +1,21 @@
 let allBuyBtns = document.querySelectorAll(".buy");
-let modelWIndowChairName = document.getElementById("chairName");
+let name = "Sam";
+let phone = "+999999999";
+let mail = "seymur2828.@mail.ru";
 
 for (let i = 0; i < allBuyBtns.length; i++) {
     allBuyBtns[i].onclick = function(e) {
         let clickedParent = e.target.parentElement
+        let modalPersonName = document.getElementById("modal_name")
+        let modalPhone = document.getElementById("modal_phone")
+        let modalMail = document.getElementById("modal_mail")
+        let chairName = document.getElementById("modal_chairName");
+
+        modalPersonName.innerHTML = `Имя: ${name}`;
+        modalPhone.innerHTML = `Телефон: ${phone}`;
+        modalMail.innerHTML = `Email: ${mail}`;
         let text = clickedParent.querySelector('.text')
-        modelWIndowChairName.innerHTML += text.innerHTML
+        chairName.innerHTML = `Название товара:<b> ${text.innerHTML}</b>`
 
         modal.classList.toggle("closed");
         modalOverlay.classList.toggle("closed");
